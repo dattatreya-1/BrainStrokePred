34cb819941236954fdd0dd44495309e043acbed8
dattatreya-1 <157103025+dattatreya-1@users.noreply.github.com>
2026-03-15 08:17:28 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
 import streamlit as st
 import numpy as np
+import cv2
 import joblib
 from PIL import Image
-from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 
-# load models
-xgb_model = joblib.load("xgboost_model.pkl")
-vgg = VGG16(weights="imagenet", include_top=False, input_shape=(224,224,3))
+st.title("Brain Stroke Prediction")
 
-labels = {
-    0: "Hemorrhagic Stroke",
-    1: "Ischemic Stroke"
-}
+model = joblib.load("xgboost_model.pkl")
 
-st.title("Brain Stroke Type Prediction")
-
-file = st.file_uploader("Upload Brain Scan Image", type=["jpg","png","jpeg"])
+file = st.file_uploader("Upload MRI", type=["jpg","png","jpeg"])
 
 if file is not None:
 
-    img = Image.open(file).convert("RGB")
-    st.image(img, caption="Uploaded Image", use_column_width=True)
+    img = Image.open(file)
+    st.image(img)
 
-    img = img.resize((224,224))
     img = np.array(img)
+    img = cv2.resize(img, (64,64))
+    img = img.flatten().reshape(1,-1)
 
-    img = preprocess_input(np.expand_dims(img, axis=0))
+    pred = model.predict(img)
 
-    features = vgg.predict(img)
-    features = features.reshape(1,-1)
-
-    pred = xgb_model.predict(features)
-
-    st.success(f"Prediction: {labels[pred[0]]}")
+    if st.button("Predict"):
+        if pred[0]==0:
+            st.error("Hemorrhagic Stroke")
+        else:
+            st.success("Ischemic Stroke")
